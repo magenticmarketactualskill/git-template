47f26592fed14b27d687e8709f2959d678b53a3c
Eric Laquer <LaquerEric@gmail.com>
2025-12-16 12:26:34 -0500

    first data-driven

--- lib/git_template/template_generators/completion_generator.rb
+++ lib/git_template/template_generators/completion_generator.rb
@@ -2,7 +2,17 @@ module GitTemplate
   module TemplateGenerators
     class CompletionGenerator
       def self.execute
-        raise "Must override"
+        text_data.each do |line_data|
+          if line_data.length == 1
+            say line_data[0]
+          else
+            say line_data[0], line_data[1]
+          end
+        end
+      end
+      
+      def self.text_data
+        raise "Must override text_data method"
       end
     end
   end
